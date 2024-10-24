b02d2e784cd62e94eed418c6d8b4d69b23b5878f
serge-conrad <serge.conrad@umontpellier.fr>
2024-10-24 18:01:12 +0200

    binary

---
+++ binary_relevance.py
@@ -0,0 +1,74 @@
+from skmultilearn.dataset import load_dataset
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import make_pipeline
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+#from sklearn.tree import DecisionTreeRegressor  # Import DecisionTreeRegressor
+from sklearn.tree import DecisionTreeClassifier  # Import DecisionTreeRegressor
+import numpy as np
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import cross_validate
+from skmultilearn.problem_transform import BinaryRelevance
+import sklearn.metrics as metrics
+from sklearn.svm import SVC
+
+#var="emotions"
+#var="enron"
+print("converts the multi-label problem into multiple independent binary classification problems (one for each label).")
+print("classifier=SVC(): The base classifier for each of these independent binary classification problems is an SVM (Support Vector Machine), implemented by SVC() from the scikit-learn library.")
+print("require_dense=[False, True], premier argument X, deuxième y")
+
+
+
+var="yeast"
+
+# Load the emotions dataset (training)
+X_train, y_train, _, _ = load_dataset(var, 'train')
+
+# Load the emotions dataset (testing)
+X_test, y_test, _, _ = load_dataset(var, 'test')
+
+
+#####################LOGISTIC REGRESSION###############################################
+# Create a pipeline with StandardScaler and LogisticRegression
+log_reg_pipeline = make_pipeline(StandardScaler(with_mean=False), LogisticRegression())
+# Initialize BinaryRelevance with the pipeline as the base classifier
+clf = BinaryRelevance(
+    classifier=log_reg_pipeline,
+    require_dense=[False, True]
+)
+clf.fit(X_train, y_train)
+
+print(clf.classifier)
+prediction = clf.predict(X_test)
+print("loss",metrics.hamming_loss(y_test, prediction))
+print("accuracy",metrics.accuracy_score(y_test, prediction))
+f1_micro = metrics.f1_score(y_test, prediction, average='micro')
+print("F1 Micro:", f1_micro)
+
+
+
+#####################SVC##############################################
+# Create a pipeline with SVC
+log_reg_pipeline = make_pipeline(StandardScaler(with_mean=False), SVC())
+# Initialize BinaryRelevance with the pipeline as the base classifier
+clf = BinaryRelevance(
+    classifier=log_reg_pipeline,
+    require_dense=[False, True]
+)
+clf.fit(X_train, y_train)
+
+print(clf.classifier)
+prediction = clf.predict(X_test)
+print("loss",metrics.hamming_loss(y_test, prediction))
+print("accuracy",metrics.accuracy_score(y_test, prediction))
+f1_micro = metrics.f1_score(y_test, prediction, average='micro')
+print("F1 Micro:", f1_micro)
+
+
+
+
+
+
+
