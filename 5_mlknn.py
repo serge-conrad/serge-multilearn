687f9b71ec150c176bb0f1b4a078918488b0f460
serge-conrad <serge.conrad@umontpellier.fr>
2024-10-25 11:02:35 +0200

    message

--- 5_mlknn.py
+++ 5_mlknn.py
@@ -42,3 +42,34 @@ f1_micro = metrics.f1_score(y_test, prediction, average='micro')
 print("F1 Micro:", f1_micro)
 
 
+from sklearn.metrics import make_scorer, f1_score
+######################################################
+# Define a scorer for multi-label classification
+f1_scorer = make_scorer(f1_score, average='micro')
+
+# Parameter grid for k and s
+param_grid = {
+    'k': [1, 3, 5, 7, 10],
+    's': [0.5, 1.0, 1.5, 2.0]
+}
+
+# Initialize MLkNN classifier without setting parameters
+classifier = MLkNN()
+
+# Perform grid search
+grid_search = GridSearchCV(classifier, param_grid, scoring=f1_scorer, cv=5)
+grid_search.fit(X_train, y_train)
+
+# Extract the best parameters
+best_params = grid_search.best_params_
+print("Best parameters found: ", best_params)
+
+# Fit classifier with best hyperparameters
+classifier = MLkNN(k=best_params['k'], s=best_params['s'])
+prediction = classifier.fit(X_train, y_train).predict(X_test)
+
+print("loss",metrics.hamming_loss(y_test, prediction))
+print("accuracy",metrics.accuracy_score(y_test, prediction))
+f1_micro = metrics.f1_score(y_test, prediction, average='micro')
+print("F1 Micro:", f1_micro)
+
